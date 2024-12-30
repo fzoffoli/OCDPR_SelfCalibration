1cbb281675d9c1ecda5534752725c950be32d9e3
Filippo Zoffoli <102168041+fzoffoli@users.noreply.github.com>
2024-12-30 16:38:30 +0100

    adding transmission kinematics

--- apps/self_calibration/JacobiansCheck.m
+++ apps/self_calibration/JacobiansCheck.m
@@ -6,15 +6,6 @@ t = 0:dt:Tmax;
 zeta_0 = [-0.5;0;0.5;0;0;0];
 zeta_1 = [0.5;0;-0.5;0;pi/6;0];
 
-zeta_d_numeric = zeros(cdpr_parameters.pose_dim,length(t));
-sigma_d_numeric = zeros(cdpr_parameters.n_cables,length(t));
-psi_d_numeric = zeros(cdpr_parameters.n_cables,length(t));
-l_d_numeric = zeros(cdpr_parameters.n_cables,length(t));
-
-sigma_d_analytic = zeros(cdpr_parameters.n_cables,length(t));
-psi_d_analytic = zeros(cdpr_parameters.n_cables,length(t));
-l_d_analytic = zeros(cdpr_parameters.n_cables,length(t));
-
 for i = 1:length(t)
     zeta(:,i) = zeta_0+(zeta_1-zeta_0)*t(i)/Tmax;
     cdpr_variables = UpdateIKZeroOrd(zeta(1:3,i),zeta(4:6,i),cdpr_parameters,cdpr_variables);
@@ -23,70 +14,15 @@ for i = 1:length(t)
         psi(j) = cdpr_variables.cable(j).tan_ang;
         l(j) = cdpr_variables.cable(j).complete_length;
     end
-    
-    %calculate numeric derivatives
-    if i~=1
-        zeta_d_numeric(:,i) = (zeta(:,i)-zeta_old)/dt;
-        sigma_d_numeric(:,i) = (sigma-sigma_old)/dt;
-        psi_d_numeric(:,i) = (psi-psi_old)/dt;
-        l_d_numeric(:,i) = (l-l_old)/dt;
-    end
+
     [flag_s,err_s] = checkGradients(@(pose)CheckSwivelFun(cdpr_variables,cdpr_parameters,pose,sigma),zeta(:,i));
     [flag_p,err_p] = checkGradients(@(pose)CheckTangencyFun(cdpr_variables,cdpr_parameters,pose,psi),zeta(:,i));
     [flag_l,err_l] = checkGradients(@(pose)CheckCableLenghtsFun(cdpr_variables,cdpr_parameters,pose,l),zeta(:,i));
     
-    %calculate analytic derivatives
-    sigma_d_analytic(:,i) = cdpr_variables.analitic_jacobian_s'*(zeta_1-zeta_0)/Tmax;
-    psi_d_analytic(:,i) = cdpr_variables.analitic_jacobian_p'*(zeta_1-zeta_0)/Tmax;
-    l_d_analytic(:,i) = cdpr_variables.analitic_jacobian_l'*(zeta_1-zeta_0)/Tmax;
-
-    %update old values for differentiation
-    zeta_old = zeta(:,i);
-    for j = 1:cdpr_parameters.n_cables
-        sigma_old(j) = cdpr_variables.cable(j).swivel_ang;
-        psi_old(j) = cdpr_variables.cable(j).tan_ang; 
-        l_old(j) = cdpr_variables.cable(j).complete_length; 
-    end
 end
 
-% figure()
-% subplot(2,1,1)
-% plot(t,zeta,'LineWidth',1.5);
-% legend('x','y','z','phi','psi','theta')
-% subplot(2,1,2)
-% plot(t,zeta_d_numeric,'LineWidth',1.5);
-% legend('x_d','y_d','z_d','phi_d','psi_d','theta_d')
+assert(flag_s,'Swivel Jacobian is wrong');
+assert(flag_p,'Tangency jacobian is wrong');
+assert(flag_l,'Cable length jacobian is wrong');
 
-figure()
-subplot(3,1,1)
-plot(t(2:end),sigma_d_numeric(:,2:end),'LineWidth',1.5);
-title('numeric')
-subplot(3,1,2)
-plot(t(2:end),sigma_d_analytic(:,2:end),'LineWidth',1.5);
-title('analytic')
-subplot(3,1,3)
-plot(t(2:end),sigma_d_analytic(:,2:end)-sigma_d_numeric(:,2:end),'LineWidth',1.5);
-title('difference')
-
-figure()
-subplot(3,1,1)
-plot(t(2:end),psi_d_numeric(:,2:end),'LineWidth',1.5);
-title('numeric')
-subplot(3,1,2)
-plot(t(2:end),psi_d_analytic(:,2:end),'LineWidth',1.5);
-title('analytic')
-subplot(3,1,3)
-plot(t(2:end),psi_d_analytic(:,2:end)-psi_d_numeric(:,2:end),'LineWidth',1.5);
-title('difference')
-
-figure()
-subplot(3,1,1)
-plot(t(2:end),l_d_numeric(:,2:end),'LineWidth',1.5);
-title('numeric');
-subplot(3,1,2)
-plot(t(2:end),l_d_analytic(:,2:end),'LineWidth',1.5);
-title('analytic');
-subplot(3,1,3)
-plot(t(2:end),l_d_analytic(:,2:end)-l_d_numeric(:,2:end),'LineWidth',1.5);
-title('difference');
 end
--- apps/self_calibration/TransmissionModelCheck.m
+++ apps/self_calibration/TransmissionModelCheck.m
@@ -3,20 +3,21 @@ close all
 clc
 
 % transmission parameters (random values)
-l_d = 80;
-R = 80;
-d_m = 40; 
+drum_length = 80;
+flag_radius = 80;
+motor_diameter = 40; 
 q = 30;
 s = 50;
-rho = 4;
+motor_pitch = 4;
 h = 40;
 
 % motor angle discretization
 N = 10000;
-theta_m = linspace(0,l_d*2*pi/rho,N);
+theta_m = linspace(0,drum_length*2*pi/motor_pitch,N);
 
 norm_PT=zeros(size(theta_m));
-[flag,error] = checkGradients(@(theta_)foo_calc_norm_PT(l_d,R,d_m,q,s,rho,h,theta_),theta_m(250));
+[flag,error] = checkGradients(@(theta_)foo_calc_norm_PT(drum_length,...
+    flag_radius,motor_diameter,q,s,motor_pitch,h,theta_),theta_m(250));
 
 figure()
 plot(norm_PT);
